fcedd8ec935bd70035d3a933d483658ec31151e0
Hao Chen <mmkrty@gmail.com>
2022-10-04 16:54:35 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        prime: "#80A4A9",
+        sec: "#39393A",
+      },
+    },
+  },
+  plugins: [],
+};
