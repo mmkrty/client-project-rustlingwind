baf89af7fbe77fbeb8b38b296aa85213aa0c1f31
Hao Chen <mmkrty@gmail.com>
2022-10-09 22:07:17 +0200

    add product page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         prime: "#80A4A9",
         sec: "#39393A",
       },
+      backgroundImage: {
+        product:
+          "url(https://scontent-ber1-1.xx.fbcdn.net/v/t39.30808-6/278449693_114844141189562_7879203020232767708_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=UDsN-V4dHBQAX-wCbyT&_nc_ht=scontent-ber1-1.xx&oh=00_AT_RPxz18aSmXJLe-9rHjrj13RUYPZpyo5MIY__rjJyEzg&oe=6347B8A2)",
+      },
     },
   },
   plugins: [],
